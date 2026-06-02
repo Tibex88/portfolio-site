130c4c081c55e93dab297b1da112e85f0f607c04
tibex88 <tibesolomon7@gmail.com>
2026-06-02 10:12:03 +0300

    fix: remove location from footer

--- src/components/PortfolioFooter.vue
+++ src/components/PortfolioFooter.vue
@@ -4,7 +4,7 @@
       <span>© 2026 tibebe solomon</span>
       <span>Design inspired by <a class="text-[var(--accent)]" href="https://roccobarbaro.com" target="_blank" rel="noopener noreferrer">Rocco Barbaro</a></span>
       
-      <span>addis ababa, ethiopia</span>
+      <!-- <span>addis ababa, ethiopia</span> -->
     </div>
   </footer>
 </template>
